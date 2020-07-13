27869764ade99175a082c1a16a411708e11befa5
Maurizio van der Werf <maurizio.vdwerf@ziggo.nl>
2020-07-13 17:44:48 +0200

    Variable Mutation and type Coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,14 +1,25 @@
-var firstName = 'John';
-console.log(firstName);
+// var firstName = 'John';
+// console.log(firstName);
 
-var lastName = 'Smith';
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+var firstName = 'John';
 var age = 28;
 
-var fullAge = true;
-console.log(fullAge);
+console.log(firstName + ' ' + age);
 
-var job;
-console.log(job);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
 
-job = 'Teacher';
-console.log(job);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
