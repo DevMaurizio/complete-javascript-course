6d21c21085a10a21abdc8fcc347e8163596533d5
Maurizio van der Werf <maurizio.vdwerf@ziggo.nl>
2020-07-15 10:04:36 +0200

    inheritance and prototype chain

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -3,3 +3,27 @@ var john = {
     yearOfBith: 1990,
     job: 'teacher'
 }
+
+var Person = function(name, yearOfBith, job) {
+    this.name = name;
+    this.yearOfBith = yearOfBith;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBith);
+} 
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('john', 1990, 'teacher');
+var Jane = new Person('Jane', 1969, 'designer');
+var Mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+Jane.calculateAge();
+Mark.calculateAge();
+
+console.log(john.lastName);
+console.log(Jane.lastName);
+console.log(Mark.lastName);
