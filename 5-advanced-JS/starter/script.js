2585543a7d135316155c011a08073122fef1170e
Maurizio van der Werf <maurizio.vdwerf@ziggo.nl>
2020-07-16 10:17:22 +0200

    add the modules

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -29,7 +29,7 @@ console.log(john.lastName);
 console.log(Jane.lastName);
 console.log(Mark.lastName);
 */
-
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -47,3 +47,62 @@ var jane = Object.create(personProto,
     yearOfBirth: { value: 1969 },
     job: { value: 'designer'}
 })
+*/
+/*
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+var obj1 = {
+    name: 'john',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age)
+console.log(obj2.age)
+
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+var years = [1990, 1956, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]))
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+var ages = arrayCalc(years, calculateAge);
+
+var fullAges = arrayCalc(years, isFullAge);
+
+console.log(ages);
+console.log(fullAges);
