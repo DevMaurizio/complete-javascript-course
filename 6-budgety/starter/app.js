3e7465e3cccce2107eb8b54914791f4732c8c88d
Maurizio van der Werf <maurizio.vdwerf@ziggo.nl>
2020-07-16 10:36:07 +0200

    add events

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,31 +1,23 @@
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
 })();
 
 var UIController = (function() {
-
 
 })();
 
 var controller = (function(budgetCtrl, UIctrl) {
 
-    var z = budgetCtrl.publicTest(5);
-
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+        console.log('It works.');
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
